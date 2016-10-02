fe5bd5abbcd80c10b8ca000ebb5f7b3de6697f0b
babobka <babobka@bk.ru>
2016-10-03 01:06:23 +0300

    Tested

--- src/main/java/ru/babobka/nodeserials/RSA.java
+++ src/main/java/ru/babobka/nodeserials/RSA.java
@@ -9,51 +9,68 @@ import java.util.Random;
  */
 public class RSA implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    private final PrivateKey privateKey;
-    private final PublicKey publicKey;
+	private static final long serialVersionUID = 1L;
+	private final PrivateKey privateKey;
+	private final PublicKey publicKey;
+	private static final String NULL_MESSAGE_ERROR = "Can not encrypt null message";
+	private static final int MIN_BITS = 256;
 
-    public RSA(int bits) {
-        if (bits >= 8) {
-            BigInteger p = BigInteger.probablePrime(bits, new Random());
-            BigInteger q = BigInteger.probablePrime(bits, new Random());
-            BigInteger n = p.multiply(q);
-            BigInteger f = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-            BigInteger e = BigInteger.probablePrime(f.bitLength() - 1, new Random());
-            while(!e.gcd(f).equals(BigInteger.ONE))
-            {
-            	e = BigInteger.probablePrime(f.bitLength() - 1, new Random());
-            }
-            BigInteger d = e.modInverse(f);
-            privateKey = new PrivateKey(n, d);
-            publicKey = new PublicKey(n, e);
-        } else throw new IllegalArgumentException("Key can not be shorter than 8 bits");
-    }
+	public RSA(int bits) {
+		if (bits >= MIN_BITS) {
+			BigInteger p = BigInteger.probablePrime(bits, new Random());
+			BigInteger q = BigInteger.probablePrime(bits, new Random());
+			BigInteger n = p.multiply(q);
+			BigInteger f = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+			BigInteger e = BigInteger.probablePrime(f.bitLength() - 1, new Random());
+			while (!e.gcd(f).equals(BigInteger.ONE)) {
+				e = BigInteger.probablePrime(f.bitLength() - 1, new Random());
+			}
+			BigInteger d = e.modInverse(f);
+			privateKey = new PrivateKey(n, d);
+			publicKey = new PublicKey(n, e);
+		} else
+			throw new IllegalArgumentException("Key can not be shorter than " + MIN_BITS + " bits");
+	}
 
-    public RSA(PrivateKey privateKey, PublicKey publicKey) {
-        this.privateKey = privateKey;
-        this.publicKey = publicKey;
-    }
+	public RSA(PrivateKey privateKey, PublicKey publicKey) {
+		this.privateKey = privateKey;
+		this.publicKey = publicKey;
+	}
 
-    public BigInteger encrypt(BigInteger m) {
-        if (publicKey != null)
-            return m.modPow(publicKey.getE(), publicKey.getN());
-        return null;
-    }
+	public BigInteger ecnrypt(String m) {
+		if (m != null) {
+			return encrypt(stringToBigInteger(m));
+		} else {
+			throw new NullPointerException(NULL_MESSAGE_ERROR);
+		}
+	}
 
-    public BigInteger decrypt(BigInteger c) {
-        if (privateKey != null)
-            return c.modPow(privateKey.getD(), privateKey.getN());
-        return null;
-    }
+	private BigInteger encrypt(BigInteger m) {
+		if (publicKey != null)
+			return m.abs().modPow(publicKey.getE(), publicKey.getN());
+		throw new NullPointerException(NULL_MESSAGE_ERROR);
+	}
 
+	public static BigInteger stringToBigInteger(String m) {
+		return new BigInteger(MathUtil.sha2(m)).abs();
+	}
 
-    public PrivateKey getPrivateKey() {
-        return privateKey;
-    }
+	public static BigInteger bytesToHashedBigInteger(byte[] bytes) {
+		return new BigInteger(bytes).abs();
+	}
 
-    public PublicKey getPublicKey() {
-        return publicKey;
-    }
+	public BigInteger decrypt(BigInteger c) {
+		if (privateKey != null)
+			return c.abs().modPow(privateKey.getD(), privateKey.getN());
+		throw new NullPointerException(NULL_MESSAGE_ERROR);
+	}
+
+	public PrivateKey getPrivateKey() {
+		return privateKey;
+	}
+
+	public PublicKey getPublicKey() {
+		return publicKey;
+	}
 
 }
